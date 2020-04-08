d7b6e6da844234686712ffa6852f176965ad1ea0
Andrew Shen <andrewshen2913@gmail.com>
2020-04-08 18:50:37 +1000

    added launchfile, listener now stops when no numbers are published

--- scripts/listener.py
+++ scripts/listener.py
@@ -4,6 +4,7 @@ import rospy
 import message_filters
 from std_msgs.msg import Int16
 
+# Global var to store the numbers published
 pub_ints = [0, 0]
 
 def isprime(num):
@@ -13,6 +14,7 @@ def isprime(num):
             return False
     return True
 
+# Callbacks store the numbers published in each topic
 def callback1(data):
     pub_ints[0] = data.data
 
@@ -25,13 +27,19 @@ def listener():
     rospy.Subscriber('topic1', Int16, callback1)
     rospy.Subscriber('topic2', Int16, callback2)
 
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10) # Listen at 10hz, the same rate as the publishers
     while not rospy.is_shutdown():
         num = max(pub_ints)
-        primes = [n for n in pub_ints if isprime(n)]
-        if len(primes) != 0: num = max(primes)
-        if num != 0:
+        primes = [n for n in pub_ints if isprime(n)] # find prime numbers
+        if len(primes) != 0: num = max(primes) # if there are prime numbers, find the biggest of those
+        # if the largest number is 0 
+        # (only possible when no numbers have been published since numbers are between 1-1000) 
+        # don't print anything
+        if num != 0: 
             rospy.loginfo(num)
+        # reset stored numbers
+        pub_ints[0], pub_ints[1] = 0, 0
+        
         rate.sleep()
 
 if __name__ == '__main__':
--- scripts/pub_rint.py
+++ scripts/pub_rint.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
+# Publishes random int between 1-1000 to topic[n]
+# Takes the first argument as the topic number
+
 import rospy, random
 from std_msgs.msg import Int16
 import sys
 
 def pub_rint():
-    topic_nb = sys.argv[1]
-    debug_nb = int(sys.argv[2]) if len(sys.argv) > 2 else None
+    argv = rospy.myargv(argv=sys.argv)
+    topic_nb = argv[1]
+    debug_nb = int(argv[2]) if len(argv) > 2 else None
 
     pub = rospy.Publisher('topic%s' % topic_nb, Int16, queue_size=20)
     rospy.init_node('pub_rint', anonymous=True)
@@ -16,7 +20,6 @@ def pub_rint():
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rint = random.randint(1, 1000)
-        #rospy.loginfo(rint)
         if debug_nb:
             pub.publish(debug_nb)
         else:
