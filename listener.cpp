a22599c7145ee6071a21c7e76be9ca3ad1e88188
Rohan Jahagirdar <rohan.australia@gmail.com>
2020-04-24 22:23:17 +1000

    bug fixes

--- listener.cpp
+++ listener.cpp
@@ -77,8 +77,8 @@ int main(int argc, char **argv) {
 
     ros::NodeHandle nh;
 
-    ros::Subscriber sub1 = nh.subscribe("topic1", 1000, &NumSelector::topic1callback, &numSelector);
-    ros::Subscriber sub2 = nh.subscribe("topic2", 1000, &NumSelector::topic2callback, &numSelector);
+    ros::Subscriber sub1 = nh.subscribe("topic1", 1000, &NumSelector::topic1CallBack, &numSelector);
+    ros::Subscriber sub2 = nh.subscribe("topic2", 1000, &NumSelector::topic2CallBack, &numSelector);
 
     int result;
     result = numSelector.process_nums();
