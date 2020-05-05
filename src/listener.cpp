f89db462d30d7f5078ca4183044ffaeb4f196e20
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-05 22:51:15 +1000

    Update listener.cpp
    Made callbacks generic to topic input name

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
 #include "std_msgs/Int16.h"
 
-void nums1Callback(const std_msgs::Int16::ConstPtr& msg)
+void topic1Callback(const std_msgs::Int16::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%d] on nums1", msg->data);
+  ROS_INFO("I heard: [%d] on %s", msg->data, argv[1]);
 }
 
-void nums2Callback(const std_msgs::Int16::ConstPtr& msg)
+void topic2Callback(const std_msgs::Int16::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%d] on nums2", msg->data);
+  ROS_INFO("I heard: [%d] on %s", msg->data, argv[2]);
 }
 
 int main(int argc, char **argv)
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub1 = n.subscribe(argv[1], 1000, nums1Callback);
-  ros::Subscriber sub2 = n.subscribe(argv[2], 1000, nums2Callback);
+  ros::Subscriber sub1 = n.subscribe(argv[1], 1000, topic1Callback);
+  ros::Subscriber sub2 = n.subscribe(argv[2], 1000, topic2Callback);
 
   ros::spin();
 
