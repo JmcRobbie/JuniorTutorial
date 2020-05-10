a1b67cec511acb3e3f3625e4fd81116673170368
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-10 14:28:24 +1000

    Update listener.cpp

--- src/listener.cpp
+++ src/listener.cpp
@@ -4,8 +4,8 @@
 class ListenerClass {
   
   public:
-    static int num1, num2, result = 0;
-    static void topicCallback(const std_msgs::Int16::ConstPtr& msg, int &num, std::string topic);
+    int num1, num2, result = 0;
+    void topicCallback(const std_msgs::Int16::ConstPtr& msg, int &num, std::string topic);
   
 };
 
@@ -24,8 +24,8 @@ int main(int argc, char **argv)
   
   ListenerClass l;
 
-  ros::Subscriber sub1 = n.subscribe("topic1", 1000, boost::bind(ListenerClass::topicCallback, _1, ListenerClass::num1, "topic1"), &l);
-  ros::Subscriber sub2 = n.subscribe("topic2", 1000, boost::bind(ListenerClass::topicCallback, _1, ListenerClass::num2, "topic2"), &l);
+  ros::Subscriber sub1 = n.subscribe("topic1", 1000, boost::bind(l::topicCallback, _1, l::num1, "topic1"), &l);
+  ros::Subscriber sub2 = n.subscribe("topic2", 1000, boost::bind(l::topicCallback, _1, l::num2, "topic2"), &l);
 
   ros::spin();
 
