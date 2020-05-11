7f187be49ae1bc0e82bf24b02b96fe98994206c9
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 17:38:02 +1000

    Update listener.cpp
    Added selector function

--- src/listener.cpp
+++ src/listener.cpp
@@ -2,6 +2,8 @@
 #include "std_msgs/Int32.h"
 #include <math.h>
 
+#define SELECT_ERR -1
+
 void callback(const std_msgs::Int32ConstPtr& msg, int* pNum, char* topic)
 {
   ROS_INFO("I heard: [%d] on %s", msg->data, topic);
@@ -21,10 +23,46 @@ bool isPrime(int num)
   return true;
 }
 
-short int selectNum(int* pNum1, int* pNum2)
+int selectNum(int num1, int num2)
 {
-  int result = 0;
-  return result;
+  int small, large, result = 0;
+  
+  /* Find which number is larger */
+  if(num1 == num2)
+  {
+    ROS_INFO("Both equal, selected %d", num1);
+    return num1;
+  }
+  else if(num1 < num2)
+  {
+    small = num1;
+    large = num2;
+  }
+  else
+  {
+    large = num1;
+    small = num2;
+  }
+  
+  /* Check for primes */
+  if(isPrime(small))
+  {
+    if(isPrime(large))
+    {
+      ROS_INFO("Both prime, selected %d", large);
+      return large;
+    }
+    else
+    {
+    ROS_INFO("Smaller is prime, selected %d", small);
+    return small;
+    }
+  }
+  else
+  {
+     ROS_INFO("No primes, selected %d", small);
+    return large;
+  }
 }
 
 int main(int argc, char **argv)
@@ -38,6 +76,8 @@ int main(int argc, char **argv)
   
   ros::Subscriber sub1 = n.subscribe<std_msgs::Int32>(argv[1], 1000, boost::bind(callback, _1, &num1, argv[1]));
   ros::Subscriber sub2 = n.subscribe<std_msgs::Int32>(argv[2], 1000, boost::bind(callback, _1, &num2, argv[2]));
+  
+  result = selectNum(num1, num2);
   
   ros::spin();
 
