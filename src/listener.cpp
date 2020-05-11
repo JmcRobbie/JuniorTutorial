25dc879f1cc4dbe055ed994ad0fadb471984ee96
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 17:39:41 +1000

    Update listener.cpp
    Added error checking

--- src/listener.cpp
+++ src/listener.cpp
@@ -2,8 +2,6 @@
 #include "std_msgs/Int32.h"
 #include <math.h>
 
-#define SELECT_ERR -1
-
 void callback(const std_msgs::Int32ConstPtr& msg, int* pNum, char* topic)
 {
   ROS_INFO("I heard: [%d] on %s", msg->data, topic);
@@ -72,12 +70,18 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  int num1, num2 = 0;
+  int num1, num2, result = 0;
   
   ros::Subscriber sub1 = n.subscribe<std_msgs::Int32>(argv[1], 1000, boost::bind(callback, _1, &num1, argv[1]));
   ros::Subscriber sub2 = n.subscribe<std_msgs::Int32>(argv[2], 1000, boost::bind(callback, _1, &num2, argv[2]));
   
+  if(num1 == 0 || num2 == 0)
+    return EXIT_FAILURE;
+  
   result = selectNum(num1, num2);
+  
+  if(result == 0)
+    return EXIT_FAILURE;
   
   ros::spin();
 
