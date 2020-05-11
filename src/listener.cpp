1925ff00ba1b61a584591142c2ccc50bba4718b1
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 16:51:49 +1000

    Update listener.cpp
    Change to int32

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,15 +1,15 @@
 #include "ros/ros.h"
-#include "std_msgs/Int16.h"
+#include "std_msgs/Int32.h"
 
-void callback(const std_msgs::Int16ConstPtr& msg, short int* pNum, char* topic)
+void callback(const std_msgs::Int32ConstPtr& msg, int* pNum, char* topic)
 {
   ROS_INFO("I heard: [%d] on %s", msg->data, topic);
   *pNum =  msg->data;
 }
 
-short int selectNum(short int* pNum1, short int* pNum2)
+short int selectNum(int* pNum1, int* pNum2)
 {
-  short int result = 0;
+  int result = 0;
   return result;
 }
 
@@ -20,10 +20,10 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  short int num1, num2 = 0;
+  int num1, num2 = 0;
   
-  ros::Subscriber sub1 = n.subscribe<std_msgs::Int16>(argv[1], 1000, boost::bind(callback, _1, &num1, argv[1]));
-  ros::Subscriber sub2 = n.subscribe<std_msgs::Int16>(argv[2], 1000, boost::bind(callback, _1, &num2, argv[2]));
+  ros::Subscriber sub1 = n.subscribe<std_msgs::Int32>(argv[1], 1000, boost::bind(callback, _1, &num1, argv[1]));
+  ros::Subscriber sub2 = n.subscribe<std_msgs::Int32>(argv[2], 1000, boost::bind(callback, _1, &num2, argv[2]));
   
   ros::spin();
 
