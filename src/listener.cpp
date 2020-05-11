3aa7ab24b53ad1a76c979877f08cb847ce0a9291
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 16:36:04 +1000

    Update listener.cpp
    Converted to argv instead of remap. Might help input arguments to callbacks

--- src/listener.cpp
+++ src/listener.cpp
@@ -3,13 +3,13 @@
 
 void topic1Callback(const std_msgs::Int16ConstPtr& msg, short int* pNum)
 {
-  ROS_INFO("I heard: [%d] on %s", msg->data, "topic1");
+  ROS_INFO("I heard: [%d] on %s", msg->data, argv[1]);
   *pNum =  msg->data;
 }
 
 void topic2Callback(const std_msgs::Int16ConstPtr& msg, short int* pNum)
 {
-  ROS_INFO("I heard: [%d] on %s", msg->data, "topic2");
+  ROS_INFO("I heard: [%d] on %s", msg->data, argv[2]);
   *pNum = msg->data;
 }
 
@@ -28,10 +28,8 @@ int main(int argc, char **argv)
 
   short int num1, num2 = 0;
   
-  ros::Subscriber sub1 = n.subscribe<std_msgs::Int16>("topic1", 1000, boost::bind(topic1Callback, _1, &num1));
-  ros::Subscriber sub2 = n.subscribe<std_msgs::Int16>("topic2", 1000, boost::bind(topic2Callback, _1, &num2));
-
-  
+  ros::Subscriber sub1 = n.subscribe<std_msgs::Int16>(argv[1], 1000, boost::bind(topic1Callback, _1, &num1));
+  ros::Subscriber sub2 = n.subscribe<std_msgs::Int16>(argv[2], 1000, boost::bind(topic2Callback, _1, &num2));
   
   ros::spin();
 
