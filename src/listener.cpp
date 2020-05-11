2cda736d8483428c22c1d597c1545658f571196a
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 16:11:22 +1000

    Update listener.cpp

--- src/listener.cpp
+++ src/listener.cpp
@@ -8,9 +8,15 @@ class ListenerClass {
   
 };
 
-void topicCallback(const std_msgs::Int16ConstPtr& msg, int &num)
+void topic1Callback(const std_msgs::Int16ConstPtr& msg, int &num)
 {
-  ROS_INFO("I heard: [%d] on topic", msg->data);
+  ROS_INFO("I heard: [%d] on %s", msg->data, "topic1");
+  num = msg->data;
+}
+
+void topic2Callback(const std_msgs::Int16ConstPtr& msg, int &num)
+{
+  ROS_INFO("I heard: [%d] on %s", msg->data, "topic2");
   num = msg->data;
 }
 
@@ -25,8 +31,8 @@ int main(int argc, char **argv)
 
   int num1, num2 = 0;
   
-  ros::Subscriber sub1 = n.subscribe<std_msgs::Int16>("topic1", 1000, boost::bind(topicCallback, _1, &num1));
-  ros::Subscriber sub2 = n.subscribe<std_msgs::Int16>("topic2", 1000, boost::bind(topicCallback, _1, &num2));
+  ros::Subscriber sub1 = n.subscribe<std_msgs::Int16>("topic1", 1000, boost::bind(topic1Callback, _1, &num1));
+  ros::Subscriber sub2 = n.subscribe<std_msgs::Int16>("topic2", 1000, boost::bind(topic2Callback, _1, &num2));
 
   ros::spin();
 
