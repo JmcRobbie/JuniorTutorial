fcf008f76f989c38ba0888db86d6e1da2d051beb
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 16:42:58 +1000

    Update talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -7,20 +7,22 @@
 
 int main(int argc, char **argv)
 {
+  
   ros::init(argc, argv, "talker");
   
   ros::NodeHandle n;
 
   ros::Publisher talker_pub = n.advertise<std_msgs::Int16>("topic", 1000);
 
+  srand(time(NULL));
+  
   ros::Rate loop_rate(FREQ);
-
+  
   int count = 0;
   while (ros::ok())
   {
     std_msgs::Int16 msg;
     
-    srand(time(NULL));
     msg.data = rand() % (MAX - MIN) + MIN;
 
     ROS_INFO("%d", msg.data);
