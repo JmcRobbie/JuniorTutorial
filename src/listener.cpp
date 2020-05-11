377fb8fa393b4e8b774547b8e2bbfc30e0dd8ec7
Cas Kent <63283172+casjkent@users.noreply.github.com>
2020-05-11 17:16:33 +1000

    Update listener.cpp

--- src/listener.cpp
+++ src/listener.cpp
@@ -13,7 +13,7 @@ bool isPrime(int num)
   if(num == 1)
     return false;
   
-  for(int i = 2, i <= sqrt(num), i++)
+  for(int i = 2; i <= sqrt(num); i++)
   {
     if(num % i == 0)
       return false;
